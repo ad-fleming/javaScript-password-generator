1b2ab19782069788635ff49d638f82ad79f0607f
ad-fleming <adfleming@gmail.com>
2020-09-02 17:25:13 -0400

    restrucured conditional statment so else{} includes the alert of in correct character range. Added an object of passwordDetails where variables will be saved and referenced

--- script.js
+++ script.js
@@ -15,29 +15,37 @@ generateBtn.addEventListener("click", writePassword);
 
 // Set password criteria variable like switches
 
-generatePassword(){
-  // TODO: GENERATE A REAL PASSWORD AND REPLACE THE RETURN STRING WITH A REAL PASSWORD STRING
-  return ("This is a placeholder password");
+// generatePassword(){
+//   // TODO: GENERATE A REAL PASSWORD AND REPLACE THE RETURN STRING WITH A REAL PASSWORD STRING
+//   return ("This is a placeholder password");
+// }
+var passwordDetails = {
+    passLength : 0,
+    includeLowerCase : false,
+    includeUpperCase : false,
+    includeNumeric : false,
+    includeSpecial : false
 }
+// var passwordLength = 0;
+// var includeLowerCase = false;
+// var includeUpperCase = false;
+// var includeNumeric = false;
+// var includeSpecial = false;
 
-var passwordLength = 0;
-var includeLowerCase = false;
-var includeUpperCase = false;
-var includeNumeric = false;
-var includeSpecial = false;
 
-// When user clicks the generate password button-->ask, if they would like to create password- if not, end. If so, begin prompts.
-var initPassword = confirm("Would you like to generate a new, secure password?");
-if(initPassword === false){
-  alert("Okay...maybe later then.");
-}else{
+// When user clicks button begin prompts.
+
   // Ask user how many characters they'd like to be. Convert the string into numeric data and update the passwordLength variable to equal this number.
  var passwordLengthString = prompt("How many characters would you like your password to be?" + "\n" + "Please keep password between 8 - 128 characters.");
-  if (parseInt(passwordLengthString) < 8 || parseInt(passwordLengthString) > 128){
-    alert("Please keep your answer within the specified amount of characters.");
+  if (parseInt(passwordLengthString) > 8 || parseInt(passwordLengthString) < 128){
+          // convert the string into a number and save to variable
+          var passwordLength = parseInt(passwordLengthString);
+          console.log(passwordLength);
+          // set the passLength to be equal to this variable in the passwordDetails object
+          passwordDetails.passLength = passwordLength;
+          console.log(passwordDetails.passLength);
   }else{
-    passwordLength = parseInt(passwordLengthString);
-    console.log(passwordLength);
-  } //Working//
-}
+    alert("Please keep your answer within the specified amount of characters.")
+  } 
+
 
