9d06cfa64529cb1b2aa991403a93a0bfb7c25ef0
ad-fleming <adfleming@gmail.com>
2020-09-02 09:14:04 -0400

    added initial variable, user prompt for password creation and a prompt asking for password length. convert the string of that response and reset inital passwordLength variable to be equal to parseInt() of the user's answer

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,27 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Set password criteria variable like switches
+
+var passwordLength = 0;
+var includeLowerCase = false;
+var includeUpperCase = false;
+var includeNumeric = false;
+var includeSpecial = false;
+
+// Ask the user if they would like to create password- if not, end. If so, begin prompts.
+var initPassword = confirm("Would you like to generate a new, secure password?");
+if(initPassword === false){
+  alert("Okay...maybe later then.");
+}else{
+  // Ask user how many characters they'd like to be. Convert the string into numeric data and update the passwordLength variable to equal this number.
+ var passwordLengthString = prompt("How many characters would you like your password to be?" + "\n" + "Please keep password between 8 - 128 characters.");
+  if (parseInt(passwordLengthString) < 8 || parseInt(passwordLengthString) > 128){
+    alert("Please keep your answer within the specified amount of characters.");
+  }else{
+    passwordLength = parseInt(passwordLengthString);
+    console.log(passwordLength);
+  } //Working//
+}
+
