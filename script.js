d374b1d1c77aeecc21940dce0df134068751ac81
ad-fleming <adfleming@gmail.com>
2020-09-02 17:46:40 -0400

    added a confirm() to asking if the user would like to include lower case characters and  passwordDetails.includeLowerCase with the result

--- script.js
+++ script.js
@@ -32,20 +32,44 @@ var passwordDetails = {
 // var includeNumeric = false;
 // var includeSpecial = false;
 
+// Dummy Steps
+// 1. Ask user fill out our 'password preferences questionnaire' 
+// 2.Use that information to fill in our 'template' with user data
+// 3.Take that data template to the password 'machine' and build it to their specifications
+// 4.Once it's built, put a label on it (define it);
+// 5.Send it back to them (display on screen)
+
 
 // When user clicks button begin prompts.
 
   // Ask user how many characters they'd like to be. Convert the string into numeric data and update the passwordLength variable to equal this number.
- var passwordLengthString = prompt("How many characters would you like your password to be?" + "\n" + "Please keep password between 8 - 128 characters.");
-  if (parseInt(passwordLengthString) > 8 || parseInt(passwordLengthString) < 128){
+ var userLengthString = prompt("How many characters would you like your password to be?" + "\n" + "Please keep password between 8 - 128 characters.");
+  if (parseInt(userLengthString) > 8 || parseInt(userLengthString) < 128){
           // convert the string into a number and save to variable
-          var passwordLength = parseInt(passwordLengthString);
+          var passwordLength = parseInt(userLengthString);
           console.log(passwordLength);
-          // set the passLength to be equal to this variable in the passwordDetails object
+          // set the passLength in the passwordDetails object to be equal to this variable in the passwordDetails object
           passwordDetails.passLength = passwordLength;
           console.log(passwordDetails.passLength);
-  }else{
+          // prompt the user if they would like to like to include lower case characters in their password         
+  }else {
     alert("Please keep your answer within the specified amount of characters.")
-  } 
+  }
+// confirm if the user would like to use lowercase
+var userLowerCase = confirm("Would you like to include lowercase characters in your password?")
+          console.log(userLowerCase);
+          // if yes, update includeLowerCase in passwordDetails object to be true;
+          if(userLowerCase === true){
+            passwordDetails.includeLowerCase = true;
+            console.log(passwordDetails.includeLowerCase) 
+            // Working
+          }else{
+            // if no, update includeLowerCase in passwordDetails object to be false;
+            if(userLowerCase === false){
+              passwordDetails.includeLowerCase = false;
+              console.log(passwordDetails.includeLowerCase)
+            }
+          }
+
 
 
